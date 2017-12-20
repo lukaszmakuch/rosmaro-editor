103a23a2f71f4b6abc05a00b59ef4cc106ebb8e9
Łukasz Makuch <zopsesen@gmail.com>
2017-12-20 19:42:46 -0400

    rosmaro-snabbdom-jest skeleton

--- src/app.js
+++ src/app.js
@@ -2,5 +2,60 @@ import cytoscape from 'cytoscape';
 import makeStorage from 'rosmaro-in-memory-storage';
 import makeLock from 'rosmaro-process-wide-lock';
 import rosmaro from 'rosmaro';
+const snabbdom = require('snabbdom');
+const patch = snabbdom.init([
+  require('snabbdom/modules/class').default,
+  require('snabbdom/modules/props').default,
+  require('snabbdom/modules/style').default,
+  require('snabbdom/modules/eventlisteners').default,
+]);
+const h = require('snabbdom/h').default;
+import graph from './graph.json'
 
-alert('hello world');
+const container = document.getElementById('app');
+
+const handlers = {
+
+  'CodeView': {
+
+    noticeCodeChange: ({code, ctx}) => ({arrow: 'CodeChanged', ctx: {...ctx, code}}),
+
+    load: () => ({arrow: 'CodeLoaded'}),
+
+    render: ({ctx, thisModel}) => h('div', {}, [
+      h('span', {}, 'paste the graph JSON file below'), h('br'),
+      h(
+        'textarea', 
+        {on: {change: e => thisModel.noticeCodeChange({code: e.target.value})}},
+        ctx.code
+      ),
+      h('br'),
+      h('button', {on: {click: () => thisModel.load()}}, 'load')
+    ])
+
+  },
+
+  'GraphEditor': {
+
+    render: ({ctx, thisModel}) => h('div', {}, [
+      h('span', {}, 'this is the graph')
+    ])
+
+  }
+
+};
+
+const appMachine = rosmaro({
+  graph,
+  handlers,
+  storage: makeStorage(),
+  lock: makeLock(),
+  afterTransition: () => refreshView()
+});
+let lastView = container;
+const refreshView = () => {
+  const newView = appMachine.render();
+  patch(lastView, newView);
+  lastView = newView;
+};
+refreshView();
---
+++ src/app.test.js
@@ -0,0 +1,3 @@
+it('converts the format Rosmaro understands to the format the Editor works with', () => {
+  expect(42).toEqual(42);
+});
